Fixed issues with formulas containing sympy operations when using units. Added testing of formulas including units.

src/pharmakin/utils/stats.py
@@ -1,5 +1,6 @@
 from functools import partial
 import numpy as np
+from typing import Callable
 
 
 def _compute_rayleigh_scale(mean, scale):
@@ -32,47 +33,83 @@ def _make_renamer(mean: str=None, scale: str=None):
     return inner
 
 
+# Map the names of some distributions (must match a numpy.Generator distribution name) to a method for
+# converting mean + scale into the required kwargs for the distribution
+_distribution_parsers = dict(
+    rayleigh=_compute_rayleigh_scale,
+    normal=_make_renamer(mean="loc", scale="scale")
+)
+
+
+def parse_distribution_mean_scale(distribution: str, mean: float=None, scale: float=None) -> dict:
+    """Takes a distribution name, and values for mean/scale. Computes the parameters needed to make a distrution
+    with the specified mean/scale.
+    The resulting dict d can be passed to numpy.Generator.<distribution>(**d) to sample."""
+
+    # Determine the correct way to parse the input mean and scale parameters
+    try:
+        parser = _distribution_parsers[distribution]
+    except KeyError:
+        dists = ", ".join(sorted(_distribution_parsers.keys()))
+        raise ValueError(f"Invalid distribution: {distribution}. Supported: {dists}.")
+
+    d = parser(mean=mean, scale=scale)
+    return d
+
+
 class Simulator:
     """Callable for simulating values drawn from various distributions, using a specified mean and possibly scale,
     depending on the distribution."""
-
-    distributions = dict(
-        rayleigh=_compute_rayleigh_scale,
-        normal=_make_renamer(mean="loc", scale="scale")
-    )
     
-    def __init__(self, distribution: str, mean: float=None, scale: float=None, seed: int=None):
+    def __init__(
+            self,
+            distribution: str,
+            lower_bound=None,
+            upper_bound=None,
+            seed: int=None,
+            **kwargs):
         """distribution is a string matching a distribution from numpy.random.Generator.<distribution>.
-        mean and scale are the desired mean and scale parameters for the distribution.
-        seed is an optional interger seed for constructing the numpy PRNG."""
+        seed is an optional interger seed for constructing the numpy PRNG.
+        lower_bound and upper_bound denote the lower and upper bounds on sampled values.
+        Values drawn outside provided bounds are truncated to the provided limits.
+        Note that this might affect the mean and scale of the distribution.
+        kwargs are keyword arguments to be passed to numpy.Generator.<distribution>"""
         
+        assert any(arg is None for arg in (lower_bound, upper_bound)) or upper_bound >= lower_bound
         self.generator = np.random.default_rng(seed=seed)
         self.distribution = distribution
-        self._func = self._make_func(mean=mean, scale=scale)
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self._func = self._make_func(**kwargs)
     
-    def _make_func(self, mean, scale):
+    def _make_func(self, **kwargs):
         """Makes a function which can be called to simulate values from the specified distribution"""
         
-        # Determine the correct way to parse the input mean and scale parameters
-        try:
-            parser = self.distributions[self.distribution]
-        except KeyError:
-            dists = ", ".join(self.distributions)
-            raise ValueError(f"Invalid distribution: {self.distribution}. Supported: {dists}.")
-    
-        # Parse mean/scale into the required parameters for the distribution
-        d = parser(mean=mean, scale=scale)
         # Create a callable which uses the PNRG to simulate values from the distribution
-        f = partial(getattr(self.generator, self.distribution), **d)
+        f = partial(getattr(self.generator, self.distribution), **kwargs)
+
         return f
     
     def __call__(self, size=None):
         """Draws a number/numbers from the distribution.
         size denotes the number of values to simulate (defaults to a single value)."""
-        return self._func(size=size)
+        
+        sample = self._func(size=size)
+        if size is None:
+            sample = [sample]
+        
+        # If bounds are provied, force values to be within bounds
+        res = np.clip(a=sample, a_min=self.lower_bound, a_max=self.upper_bound)
+        
+        # If sampling a single value, convert back to native data types (for consistency with numpy.Generator.<dist>)
+        if size is None:
+            res = res[0].item()
+        
+        return res
 
 
 if __name__ == '__main__':
-    simulate = Simulator("rayleigh", mean=2.0)
-    print(np.mean(simulate()))
+    d = parse_distribution_mean_scale("rayleigh", mean=2.0)
+    simulate = Simulator("rayleigh", **d)
+    print(simulate())
     